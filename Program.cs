3639380fdff5d26112aaa2c8b411c1c2d0a6e6dd
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-11-04 15:13:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void PrintDigitName(int digit)
+    {
+        string[] digitNames = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        Console.WriteLine(digitNames[digit]);
+    }
+
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        string numStr = num.ToString();
+
+        for (int i = numStr.Length - 1; i >= 0; i--)
+        {
+            int digit = int.Parse(numStr[i].ToString());
+            PrintDigitName(digit);
+        }
+    }
+}
